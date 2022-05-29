f24b9160f6bf618172b679236aa9a61ec32e30ad
parsec93 <52687000+parsec93@users.noreply.github.com>
2022-05-29 23:09:46 +0900

    Add files via upload

---
+++ src/main/java/com/parsec93/pasrec93/Pasrec93Application.java
@@ -0,0 +1,13 @@
+package com.parsec93.pasrec93;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pasrec93Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pasrec93Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/parsec93/pasrec93/Pasrec93ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsec93.pasrec93;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pasrec93ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
